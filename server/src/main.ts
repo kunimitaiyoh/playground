14448c1314b221514fb6a18b50454b71a4b6eea8
KUNIMI Taiyoh <kunimi.taiyoh@gmail.com>
2020-07-10 15:44:37 +0900

    (WIP) implementing POST /messages.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,7 +1,13 @@
+import { json } from "body-parser";
 import express from "express";
+
 import { RandomIntEmitter } from "./emitter";
+import { allowOrigin } from "./util";
 
 const app = express();
+app.use(json());
+app.use(allowOrigin);
+
 const emitter = new RandomIntEmitter();
 
 app.get("/", (req, res) => {
@@ -25,4 +31,21 @@ app.get("/messages", (req, res) => {
     });
 });
 
+app.post("/messages", (req, res) => {
+    const body: UserMessageRequest = req.body
+    console.log({ event: "MESSAGE_POST", body });
+
+    res.status(200).end();
+});
+
 app.listen(8080);
+
+interface UserMessageRequest {
+    roomId: string;
+    username: string;
+    body: string;
+}
+
+interface UserMessage extends UserMessageRequest {
+    created: string;
+}
